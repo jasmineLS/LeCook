00e497a7a9b76bf44b886d438918b2a999433195
Drew Arocha <aarocha.aa@gmail.com>
2023-02-25 13:10:35 -0500

    Added react-navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const HomeScreen = () =>{
+    return(
+
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
