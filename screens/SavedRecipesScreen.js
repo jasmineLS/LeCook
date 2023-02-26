18f766035b7f492a1e1ac3d7d7fa0ccb64b5df21
Tyler <schultty@mail.uc.edu>
2023-02-26 08:00:36 -0500

    adds scrolling

--- screens/SavedRecipesScreen.js
+++ screens/SavedRecipesScreen.js
@@ -1,19 +1,23 @@
-import { View, Text, FlatList, Pressable, Image } from "react-native";
+import { View, Text, FlatList, Pressable, Image, ScrollView } from "react-native";
 import { useContext } from "react";
 import AppContext from "../AppContext";
 import { Linking } from "react-native";
+import { useState } from "react";
 //import console = require("console");
 const SavedRecipesScreen = () => {
   const myContext = useContext(AppContext);
+  const [savedItems, setSavedItems] = useState(myContext.savedItems);
+
   const Saved = (item)=>{
       return(
           <Text>{item}</Text>
       )
   }
   return(
+  <ScrollView>
   <View style={{flex:1, backgroundColor:"white", }}>
-    {console.log(myContext.savedItems)}
-    {myContext.savedItems.length > 0 ? myContext.savedItems.map((item)=>{
+    {console.log(savedItems)}
+    {myContext.savedItems.length > 0 ? savedItems.map((item)=>{
       item = JSON.parse(item);
         return(
             <View style={{padding: 5,
@@ -27,11 +31,12 @@ const SavedRecipesScreen = () => {
               alignContent: 'center',}}>
               <Image source={{uri:item.image}}style={{"width":100,"height":100, borderRadius:10}}/>
              <Text style={{color:'white', fontSize:20, marginLeft:10, width:200,}} onPress={()=>{Linking.openURL(item.url)}}>{item.label}</Text>
-             <Pressable><Text style={{color:"white", fontSize:30, marginLeft:30}} onPress={()=>{}}>⤫</Text></Pressable>
+             <Pressable><Text style={{color:"white", fontSize:30, marginLeft:30}}>⤫</Text></Pressable>
             </View>
             );
-    }): <Text>None Saved</Text>}
+    }) : <Text>None Saved</Text>}
   </View>
+  </ScrollView>
   );
 };
 
