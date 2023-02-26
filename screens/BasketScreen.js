c36874874d37d4c8ae4a7624bc908e9b75e9038f
J L Smith <jasminelimsmith@jasmines-mbp-2.wifi.uc.edu>
2023-02-25 23:11:15 -0500

    added styling

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -17,13 +17,13 @@ const BasketScreen = ({navigation }) => {
   const PantryItem = item => {
     return (
       <View>
-        <Text>{item.item}</Text>
+        <Text style ={styles.list}>{item.item}</Text>
       </View>
     );
   };
 
   return (
-    <View>
+    <View style ={styles.body}>
       <TextInput
         style={styles.input}
         onChangeText={curr_text => setText(curr_text)}
@@ -38,7 +38,7 @@ const BasketScreen = ({navigation }) => {
       <Pressable
         onPress={() => navigation.navigate("Recipes", { basket: basket })}
       >
-        <Text>Find a recipe!</Text>
+        <Text style={styles.texts}>Find a recipe!</Text>
       </Pressable>
     </View>
   );
@@ -49,7 +49,29 @@ const styles = StyleSheet.create({
     height: 40,
     margin: 12,
     borderWidth: 1,
-    padding: 10
+    padding: 10,
+    color:'#fff',
+    
+  },
+  body:{
+    backgroundColor:'#6b8e23',
+    color:'#fff',
+    flex:1,
+  } ,
+  texts: {
+    textAlign: "center",
+    backgroundColor:'#6b8e23',
+    color:'#ffa500',
+    fontSize:30,
+    padding: 10,
+    borderWidth: 5,
+  },
+  list:{
+    color:'#fff',
+    height: 40,
+    margin: 2,
+    padding: 2,
+
   }
 });
 
