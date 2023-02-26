aec03347cf6f5a4a46d10afd6b6541fa0a78106a
Drew Arocha <aarocha.aa@gmail.com>
2023-02-25 22:25:27 -0500

    added login screen

--- screens/BasketStackScreen.js
+++ screens/BasketStackScreen.js
@@ -1,4 +1,3 @@
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import BasketScreen from './BasketScreen'
 import RecipesScreen from './RecipesScreen';
@@ -7,7 +6,7 @@ const BasketStack =  createStackNavigator();
 
 const BasketStackScreen = ()=>{
     return(
-        <BasketStack.Navigator>
+        <BasketStack.Navigator screenOptions={{headerShown: false}}>
             <BasketStack.Screen name="Basket" component={BasketScreen}/>
             <BasketStack.Screen name="Recipes" component={RecipesScreen}/>
         </BasketStack.Navigator>
